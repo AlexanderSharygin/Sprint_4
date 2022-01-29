f1af23c85dd3ca3462de3294ea684d453bad538f
= <AlexSharygin1989@yandex.ru>
2022-01-29 20:08:33 +0000

    Tests are implemented

--- src/main/java/ru/praktikum/string_cheker/Account.java
+++ src/main/java/ru/praktikum/string_cheker/Account.java
@@ -3,14 +3,14 @@ package ru.praktikum.string_cheker;
 public class Account {
 
     private final String name;
+    private final String nameValidationRegex = "(^[A-ZА-ЯЁ][A-zА-яЁё'-]*+)( )([A-ZА-ЯЁ][A-zА-яЁё'-]*+$)";
 
     public Account(String name) {
         this.name = name;
     }
 
     public boolean checkNameToEmboss() {
-
-        return (name.length()<20 && name.matches("(^[A-zА-яЁё]+)( )([A-zА-яЁё]+$)"));
+        return (name.length() <= 19
+                && name.matches(nameValidationRegex));
     }
-
 }
--- src/main/java/ru/praktikum/string_cheker/Praktikum.java
+++ src/main/java/ru/praktikum/string_cheker/Praktikum.java
@@ -6,15 +6,14 @@ public class Praktikum {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String nameToCheck = "";
         do {
             System.out.println("Веедите имя владельца карты и нажмиите enter:");
-            nameToCheck = in.nextLine();
+            String nameToCheck = in.nextLine();
             Account account = new Account(nameToCheck);
             if (account.checkNameToEmboss()) {
                 System.out.println("Успех. Имя владельца карты прошло валидацию.");
             } else {
-                System.out.println("Внимание!!! Введённое имя не может быть напечатано на карте.");
+                System.out.println("Внимание!!! Введённое имя не валидно и может быть напечатано на карте.");
             }
             System.out.println("Что бы провертить ещё одно имя нажмите Enter. Что бы выйти введите Quit и нажмите Enter.");
         }
